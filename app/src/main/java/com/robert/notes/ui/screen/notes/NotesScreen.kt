9cc6773adf6ce64b4fea4af800f822a35793585a
Robert Mathew <robert.c.mathew@gmail.com>
2024-01-19 15:03:11 +0530

    Added toolbar and floating action button in note screen

--- app/src/main/java/com/robert/notes/ui/screen/notes/NotesScreen.kt
+++ app/src/main/java/com/robert/notes/ui/screen/notes/NotesScreen.kt
@@ -1,23 +1,35 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.robert.notes.ui.screen.notes
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.robert.notes.R
 import com.robert.notes.data.local.Note
 import com.robert.notes.ui.Screen
 
@@ -25,7 +37,6 @@ import com.robert.notes.ui.Screen
 fun ListScreen(
     navController: NavController,
     viewModel: NotesViewModel = hiltViewModel(),
-    modifier: Modifier = Modifier
 ) {
 //    viewModel.addNote(Note(title = "A big title 2", note = "This is a big note. So this should be very big. It should be eye popping", createdAt = "22-10-2023", updatedAt = "23-10-2023", id = null))
 
@@ -34,23 +45,39 @@ fun ListScreen(
 
     val noteList: List<Note> by viewModel.noteList.observeAsState(initial = listOf())
 
-    LazyColumn(
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 16.dp), state = lazyListState
-    ) {
-        items(items = noteList) { note ->
-            ListItem(
-                note = note,
-                onNoteClick = { note -> navController.navigate(Screen.EditScreen.withArgs(note.id.toString())) })
+    Scaffold(topBar = { NoteTopAppBar() }, floatingActionButton = {
+        FloatingActionButton(onClick = { }) {
+            Icon(imageVector = Icons.Filled.Add, contentDescription = "add")
+        }
+    }) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier.padding(innerPadding),
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 16.dp),
+            state = lazyListState
+        ) {
+            items(items = noteList) { note ->
+                ListItem(note = note,
+                    onNoteClick = { note -> navController.navigate(Screen.EditScreen.withArgs(note.id.toString())) })
+            }
         }
     }
+}
 
+@Composable
+fun NoteTopAppBar() {
+    TopAppBar(
+        colors = topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = { Text(text = stringResource(id = R.string.app_name)) },
+        modifier = Modifier.fillMaxWidth()
+    )
 }
 
 @Composable
 fun ListItem(note: Note, onNoteClick: (Note) -> Unit) {
-    Column(
-        modifier = Modifier.clickable { onNoteClick(note) }
-    ) {
+    Column(modifier = Modifier.clickable { onNoteClick(note) }) {
         Text(
             text = note.title,
             style = MaterialTheme.typography.titleLarge,
